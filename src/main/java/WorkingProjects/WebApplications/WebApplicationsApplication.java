aedaec9ed3a51b0a8e25f34c576314494e340bc9
Anukam <anukams@amazon.com>
2023-02-03 12:52:43 -0500

    initial commit

---
+++ src/main/java/WorkingProjects/WebApplications/WebApplicationsApplication.java
@@ -0,0 +1,13 @@
+package WorkingProjects.WebApplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WorkingProjects/WebApplications/WebApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package WorkingProjects.WebApplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
